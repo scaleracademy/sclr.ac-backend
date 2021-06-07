46ad2c7106a4645657a6d0a9d747e3aef74102bb
Aman Anand <7aman7anand7@gmail.com>
2021-06-07 10:08:20 +0530

    model created to store short URL codes in db
    Signed-off-by: Aman Anand <7aman7anand7@gmail.com>

---
+++ models/shortURL.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const shortenURLSchema = new mongoose.Schema({
+    url: {
+        type: String,
+        required: true
+    },
+    shortCode: {
+        type: String,
+        require: true
+    },
+    visits: {
+        type: Number,
+        default: 0
+    },
+    lastVisited: {
+        type: Number,
+        default: Date.now()
+    }
+})
+
+const ShortenURL = mongoose.model('ShortenURL', shortenURLSchema);
+
+module.exports = {ShortenURL}
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 6767;
+const {db} = require("./config/mongodb")
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+
+app.listen(PORT, () => {
+    console.log("server started");
+})
