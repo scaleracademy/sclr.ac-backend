a5d2a07affde7eef376e3ccd8fd5dc32350d58a1
Kartik Kwatra <kartikkwatra2016@gmail.com>
2021-07-11 14:47:06 +0530

    Update run.ts

--- src/run.ts
+++ src/run.ts
@@ -5,7 +5,7 @@ import { connect } from './db/connect'
 (async function start() {
 
     await connect()
-    const env = process.env.PORT || 3000
+    const env = process.env.PORT || 3000;
     app.listen(env, () => {
         console.log(`Server started on http://localhost:${env}`)
     })
